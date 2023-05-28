9ca24e3c1a73d1c1550acce432a3bd28b88aa9e5
sayem mahmud <sayem.mahmud97@gmail.com>
2023-05-28 19:48:20 +0600

    k

--- components/helpers/Pagination/Pagination.tsx
+++ components/helpers/Pagination/Pagination.tsx
@@ -20,7 +20,8 @@ const Pagination: React.FC<Props> = ({pageCount, currentPage}) => {
   const [arrayStart, setArrayStart] = useState(0);
   const [arrayEnd, setArrayEnd] = useState(2);
   const [clicked, setClicked] = useState<number | null>(null)
-  const [lPclick, setLPClicked]=useState(false)
+  const [lPclick, setLPClicked] = useState(false)
+  const [lPclickL, setLPClickedL]=useState(false)
 
   const [selectedPage, setSelectedPge] = useState(0);
   // const [startShowData, setStartShowData] = useState(0);
@@ -81,15 +82,27 @@ const Pagination: React.FC<Props> = ({pageCount, currentPage}) => {
       
           {/* <div className="flex-1 text-center"> */}
           <div className="flex flex-row">
-          {currentPage >2 &&
+          <div className="flex flex-row gap-x-[7px] md:gap-x-[50px]">
+          <div
+             className={`cursor-pointer ${currentPage === 1 && "hidden"} rounded-[3px] text-white bg-black p-[5px]`}
+             onClick={() => {
+              setClicked(0)
+              handlePageClick(currentPage - 1)
+              }}
+              >PREV</div>
+              <div>
+              {currentPage >2 &&
               <div className="flex">
-                 <p onClick={() => { setLPClicked(true); handlePageClick(1) }} className={`w-[32px] cursor-pointer border-[1px] border-[#C0C0C0] flex justify-center items-center h-[32px] hover:bg-[#C0C0C0] rounded-[3px] ${lPclick && 'text-[#555252] font-bold'}`}>
+                 <p onClick={() => { setLPClicked(true); handlePageClick(1) }} className={`w-[32px] text-black cursor-pointer border-[1px] border-[#C0C0C0] flex justify-center items-center h-[32px] hover:bg-[#C0C0C0] rounded-[3px] ${lPclick && ' font-bold'}`}>
                   {/* {pageCount} */}
                   1
                 </p>
-                <p>&nbsp;&nbsp;......&nbsp;&nbsp;</p>
-              </div>}
-            <div className="flex flex-row gap-x-[8px] md:gap-x-[20px] justify-center  m-auto h-full">
+                <p className="text-black">&nbsp;&nbsp;......&nbsp;&nbsp;</p>
+              </div>
+                }
+                </div>
+              </div>
+              <div className="flex flex-row gap-x-[8px] md:gap-x-[20px] justify-center  m-auto h-full">
               {showPaginateNumberArray
                 ?.slice(arrayStart, arrayEnd)
                 // ?.slice(0, 2)
@@ -107,51 +120,47 @@ const Pagination: React.FC<Props> = ({pageCount, currentPage}) => {
                         }}
                         className="cursor-pointer"
                       >
-                        <p
-                          className={`w-[32px] flex border-[1px] border-[#C0C0C0] justify-center items-center h-[32px] ${
-                            currentPage === data+1 ?
-                            "text-white rounded-[3px] bg-psclightteal font-bold"
-                          :'hover:bg-[#C0C0C0]'} ${
-                            clicked === ind &&
+                         <p
+                           className={`w-[32px] flex border-[1px]  border-[#C0C0C0] justify-center items-center h-[32px] ${currentPage === data + 1 ?
+                            "text-white rounded-[3px] bg-psclightblack font-bold"
+                            : 'hover:bg-[#C0C0C0] text-psclightblack'} ${clicked === ind &&
                             "text-[#525455] font-bold"
                             }`
-                          }
-                        >
+                            }
+                         >
                           {data + 1}
-                        </p>
+                          </p>
                       </div>
                     </>
                   );
                 })}
             </div>
-            {currentPage !== pageCount && pageCount>2 &&
-              <div className="flex">
-                <p>&nbsp;&nbsp;......&nbsp;&nbsp;</p>
-                <p onClick={() => { setLPClicked(true); handlePageClick(pageCount) }} className={`w-[32px] cursor-pointer border-[1px] border-[#C0C0C0] flex justify-center items-center h-[32px] hover:bg-[#C0C0C0] rounded-[3px] ${lPclick && 'text-[#555252] font-bold'}`}>
-                  {pageCount}
+            {currentPage !== pageCount && pageCount > 2 &&
+                <div className="flex">
+                <p className="text-black">&nbsp;&nbsp;......&nbsp;&nbsp;</p>
+                <p onClick={() => { setLPClickedL(true); handlePageClick(pageCount) }} className={`w-[32px] text-black cursor-pointer border-[1px] border-[#C0C0C0] flex justify-center items-center h-[32px] hover:bg-[#C0C0C0] rounded-[3px] ${lPclickL && 'font-bold'}`}>
+                {pageCount}
                 </p>
-              </div>}
+                </div>}
           </div>
           <div className="flex flex-wrap gap-x-[5px] gap-y-[5px] text-white h-[32px]">
-            <div
+            {/* <div
               className={`cursor-pointer ${currentPage === 1 && "invisible"} rounded-[3px] bg-black p-[5px]`}
               onClick={() => {
                 setClicked(0)
                 handlePageClick(currentPage - 1)
                 // pageChange(selectedPage - 1, selectedDta - 1);
               }}
-            >PREV</div>
-            <div
-              className={`cursor-pointer ${
-                currentPage === pageCount && "invisible"} rounded-[3px] bg-black p-[5px]`}
-              onClick={() => {
+            >PREV</div> */}
+           <div
+              className={`cursor-pointer ${currentPage === pageCount && "invisible"} rounded-[3px] bg-black p-[5px]`}
+               onClick={() => {
                 if (currentPage == 1) {
-                  setClicked(1)
-                }
+                setClicked(1)
+                 }
                 handlePageClick(currentPage + 1)
-                // pageChange(selectedPage + 1, selectedDta + 1);
-              }}
-            >NEXT</div>
+                }}
+                >NEXT</div>
           </div>
           {/* <div
             onClick={() => {
