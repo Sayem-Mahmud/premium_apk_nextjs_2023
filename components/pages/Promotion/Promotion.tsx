cefb03c84b0eb6bc2f16ab7ca678a9d2508cc57d
sayem mahmud <sayem.mahmud97@gmail.com>
2023-06-03 16:10:19 +0600

    k

--- components/pages/Promotion/Promotion.tsx
+++ components/pages/Promotion/Promotion.tsx
@@ -13,11 +13,11 @@ const Promotion: React.FC<Props> = ({dataPromo}) => {
     const states = useSelector(() => controller.states)
     
     return (
-        <div className='my-[20px] container-x'>
-             <div className='flex h-[48px]  mb-[30px] '>
+        <div className=' container-x pt-[5px]'>
+            <div className='flex h-[48px] my-[20px] '>
                 <div className='w-[4px] h-full bg-[#2f4f4f] rounded-r'></div>
                 <div className=' flex items-center text-[25px] pl-[10px]  text-black font-bold ' >Promotions</div>
-                </div>    
+            </div>    
             <div className=' mb-[40px] pb-[40px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-7 gap-y-10 md:gap-y-10'>
                               
                           {
